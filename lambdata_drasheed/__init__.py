6791d3fcc8b54da175917a9e42d91e823b410d4b
daanishrasheed <daanishrasheed@gmail.com>
2019-12-03 12:31:06 -0500

    two functions added

--- lambdata_drasheed/__init__.py
+++ lambdata_drasheed/__init__.py
@@ -1,12 +1,35 @@
 """lambdata - collection of data science helper functions"""
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-#sample code
+# sample code
 ONES = pd.DataFrame(np.ones(10))
 ZEROS = pd.DataFrame(np.zeros(50))
 
-#sample functions
+
+# sample functions
+
+
 def increment(x):
-    return (x+1)
+    return x + 1
+
+
+def split_date(df):
+    df = df.copy()
+    df['Year'] = df['Date'].dt.year
+    df['Month'] = df['Date'].dt.month
+    df['Day'] = df['Date'].dt.day
+    return df
+
+
+def train_validate_test_split(df, train_percent=.6, validate_percent=.2, seed=None):
+    np.random.seed(seed)
+    perm = np.random.permutation(df.index)
+    m = len(df.index)
+    train_end = int(train_percent * m)
+    validate_end = int(validate_percent * m) + train_end
+    train = df.iloc[perm[:train_end]]
+    validate = df.iloc[perm[train_end:validate_end]]
+    test = df.iloc[perm[validate_end:]]
+    return train, validate, test
