5cee2721287b7c0d16dcc1f807a0e1fac81e4d68
daanishrasheed <daanishrasheed@gmail.com>
2019-12-03 13:06:51 -0500

    change files

---
+++ lambdata_drasheed/df_utils.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+def split_date(df):
+    df = df.copy()
+    df['Year'] = df['Date'].dt.year
+    df['Month'] = df['Date'].dt.month
+    df['Day'] = df['Date'].dt.day
+    return df
+
+
+def train_validate_test_split(df, train_percent=.6, validate_percent=.2, seed=None):
+    np.random.seed(seed)
+    perm = np.random.permutation(df.index)
+    m = len(df.index)
+    train_end = int(train_percent * m)
+    validate_end = int(validate_percent * m) + train_end
+    train = df.iloc[perm[:train_end]]
+    validate = df.iloc[perm[train_end:validate_end]]
+    test = df.iloc[perm[validate_end:]]
+    return train, validate, test
