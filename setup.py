aeb37dcb12a5a51445b4f7dadd9e26a395b98cdb
daanishrasheed <daanishrasheed@gmail.com>
2019-12-03 13:31:01 -0500

    setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIRED = [
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
-    name="lambdata-DS9",
+    name="lambdata-drasheed",
     version = "0.1.1",
     author = "alekslovesdata",
     description = "a collection of data science helper functions",
