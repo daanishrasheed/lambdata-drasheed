da57779586a0209e3bd2fe011742d34a5294c022
daanishrasheed <daanishrasheed@gmail.com>
2019-12-03 13:12:08 -0500

    more file changes

--- lambdata_drasheed/__init__.py
+++ lambdata_drasheed/__init__.py
@@ -13,23 +13,3 @@ ZEROS = pd.DataFrame(np.zeros(50))
 
 def increment(x):
     return x + 1
-
-
-def split_date(df):
-    df = df.copy()
-    df['Year'] = df['Date'].dt.year
-    df['Month'] = df['Date'].dt.month
-    df['Day'] = df['Date'].dt.day
-    return df
-
-
-def train_validate_test_split(df, train_percent=.6, validate_percent=.2, seed=None):
-    np.random.seed(seed)
-    perm = np.random.permutation(df.index)
-    m = len(df.index)
-    train_end = int(train_percent * m)
-    validate_end = int(validate_percent * m) + train_end
-    train = df.iloc[perm[:train_end]]
-    validate = df.iloc[perm[train_end:validate_end]]
-    test = df.iloc[perm[validate_end:]]
-    return train, validate, test
